66282f9815a25d99e66275545da49f549743ad09
Guilherme Sehn <guilherme@hey.com>
2022-09-17 19:30:37 -0300

    add players

--- lib/phoenix_live_draw_web/live/room_live.ex
+++ lib/phoenix_live_draw_web/live/room_live.ex
@@ -1,13 +1,22 @@
 defmodule PhoenixLiveDrawWeb.RoomLive do
   use PhoenixLiveDrawWeb, :live_view
 
-  alias PhoenixLiveDraw.Game.Room
+  alias PhoenixLiveDraw.Game.{Player, Room}
+  alias __MODULE__.PlayersComponent
 
   def mount(%{"id" => room_id}, _session, socket) do
-    {:ok, assign(socket, :room, Room.new(room_id))}
+    players = %{
+      "1" => Player.new("1", "John"),
+      "2" => Player.new("2", "Mari"),
+      "3" => Player.new("3", "Richard"),
+      "4" => Player.new("4", "Adam")
+    }
+
+    room = %{Room.new(room_id) | players: players}
+
+    {:ok, assign(socket, :room, room)}
   end
 
-  @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
     <div class="w-[900px] h-[572px] m-auto flex flex-row gap-3">
@@ -22,7 +31,7 @@ defmodule PhoenixLiveDrawWeb.RoomLive do
       </div>
 
       <div class="grow bg-white rounded rounded-tr-3xl rounded-br-3xl shadow-md overflow-auto break-all">
-        players
+        <PlayersComponent.list room={@room} />
       </div>
     </div>
 
--- lib/phoenix_live_draw_web/live/room_live/players_component.ex
+++ lib/phoenix_live_draw_web/live/room_live/players_component.ex
@@ -1,2 +1,20 @@
 defmodule PhoenixLiveDrawWeb.RoomLive.PlayersComponent do
+  use Phoenix.Component
+
+  def list(assigns) do
+    ~H"""
+    <ul class="divide-y border-b text-xs">
+      <%= for {_, player} <- @room.players do %>
+        <li class="p-2">
+          <div class="text-gray-900 font-medium">
+            <%= player.name %>
+          </div>
+          <div class="text-gray-500">
+            <%= player.points %> points
+          </div>
+        </li>
+      <% end %>
+    </ul>
+    """
+  end
 end
