1bac4cc93b767563b498c62e978fda45060bb318
Guilherme Sehn <guilherme@hey.com>
2022-10-02 19:02:54 -0300

    add ts files to tailwind

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -4,7 +4,12 @@
 let plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
+  content: [
+    "./js/**/*.js",
+    "./js/**/*.ts",
+    "../lib/*_web.ex",
+    "../lib/*_web/**/*.*ex",
+  ],
   theme: {
     extend: {},
   },
