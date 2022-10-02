145c4a702386e6bfd4345edecabfb4f6bb92f847
Guilherme Sehn <ghsehn@gmail.com>
2022-10-02 16:50:55 -0300

    Add countdown component (#5)
    * re-format app.js with prettier
    
    * add countdown component

--- lib/phoenix_live_draw_web/live/room_live/stage/post_round_component.ex
+++ lib/phoenix_live_draw_web/live/room_live/stage/post_round_component.ex
@@ -4,10 +4,13 @@ defmodule PhoenixLiveDrawWeb.RoomLive.Stage.PostRoundComponent do
   alias PhoenixLiveDraw.Game.Room
   alias PhoenixLiveDraw.Game.State.PostRound
 
+  import PhoenixLiveDrawWeb.CountdownComponent
+
   def render(%{room: %Room{state: %PostRound{outcome: :no_hits}}} = assigns) do
     ~H"""
     <main>
       <.title>Nobody hit the answer</.title>
+      <.post_round_countdown room={@room} />
     </main>
     """
   end
@@ -17,6 +20,7 @@ defmodule PhoenixLiveDrawWeb.RoomLive.Stage.PostRoundComponent do
     <main>
       <.title>Some people hit the answer!</.title>
       <.subtitle>The word was: <%= word_was %></.subtitle>
+      <.post_round_countdown room={@room} />
     </main>
     """
   end
@@ -26,7 +30,14 @@ defmodule PhoenixLiveDrawWeb.RoomLive.Stage.PostRoundComponent do
     <main>
       <.title>Everybody hit the answer!</.title>
       <.subtitle>The word was: <%= word_was %></.subtitle>
+      <.post_round_countdown room={@room} />
     </main>
+    """
+  end
+
+  defp post_round_countdown(assigns) do
+    ~H"""
+    <.bottom_countdown id={"#{@room.state.outcome}_countdown"} until={@room.state.expires_at} />
     """
   end
 end
