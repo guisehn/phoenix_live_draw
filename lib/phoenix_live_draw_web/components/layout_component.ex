b257a5fcb16c80cde56c8b1d8f01452e6096b62c
Guilherme Sehn <guilherme@hey.com>
2022-10-15 20:06:16 -0300

    refactor layout components

--- lib/phoenix_live_draw_web/components/layout_component.ex
+++ lib/phoenix_live_draw_web/components/layout_component.ex
@@ -1,26 +1,44 @@
 defmodule PhoenixLiveDrawWeb.LayoutComponent do
   use Phoenix.Component
 
+  import PhoenixLiveDrawWeb.ComponentUtils, only: [assign_rest: 2]
+
   def title(assigns) do
+    assigns =
+      assigns
+      |> assign_new(:class, fn -> nil end)
+      |> assign_rest(~w(class))
+
     ~H"""
-    <h1 class={"font-bold text-xl #{assigns[:class]}"}><%= render_slot(@inner_block) %></h1>
+    <h1 class={["font-bold text-xl", @class]} {@rest}><%= render_slot(@inner_block) %></h1>
     """
   end
 
   def subtitle(assigns) do
+    assigns =
+      assigns
+      |> assign_new(:class, fn -> nil end)
+      |> assign_rest(~w(class))
+
     ~H"""
-    <p class={"text-gray-500 #{assigns[:class]}"}><%= render_slot(@inner_block) %></p>
+    <p class={["text-gray-500", @class]} {@rest}><%= render_slot(@inner_block) %></p>
     """
   end
 
   def button(assigns) do
-    class = [
-      "font-bold py-2 px-4 rounded-full bg-indigo-500 text-white transition hover:bg-indigo-600 active:bg-indigo-800",
-      assigns[:class]
-    ]
+    assigns =
+      assigns
+      |> assign_new(:class, fn -> nil end)
+      |> assign_rest(~w(class))
 
     ~H"""
-    <button {assigns_to_attributes(assigns, [:class])} class={class}>
+    <button
+      class={[
+        "font-bold py-2 px-4 rounded-full bg-indigo-500 text-white transition hover:bg-indigo-600 active:bg-indigo-800",
+        @class
+      ]}
+      {@rest}
+    >
       <%= render_slot(@inner_block) %>
     </button>
     """
