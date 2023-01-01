d67ea9fddb761b19bd7d2fb405d95469b1440599
Guilherme Sehn <guilherme@hey.com>
2023-01-01 17:38:39 -0300

    add room tests

--- test/phoenix_live_draw/game/room_test.exs
+++ test/phoenix_live_draw/game/room_test.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixLiveDraw.Game.RoomTest do
   use ExUnit.Case, async: true
 
-  alias PhoenixLiveDraw.Game.{Room, Player, State}
+  alias PhoenixLiveDraw.Game.{Room, Player, PlayerMessage, PubSub, State}
 
   test "new/1" do
     room = Room.new("cb594c62")
@@ -70,6 +70,72 @@ defmodule PhoenixLiveDraw.Game.RoomTest do
 
       assert %Player{id: "5b2917a5", name: "bar", points: 10} = updated_room.players["5b2917a5"]
       assert %Player{id: "7c5b82f5", name: "gui", points: 0} = updated_room.players["7c5b82f5"]
+    end
+  end
+
+  describe "next_round_player/1" do
+    test "returns first player when there is no round player" do
+      players = %{
+        "foo" => Player.new("foo", "foo"),
+        "bar" => Player.new("bar", "bar")
+      }
+
+      room = %{Room.new("room_id") | players: players}
+
+      assert Room.next_round_player(room) == players["foo"]
+    end
+
+    test "returns next player" do
+      players = %{
+        "foo" => Player.new("foo", "foo"),
+        "bar" => Player.new("bar", "bar")
+      }
+
+      room = %{Room.new("room_id") | players: players, round_player: players["foo"]}
+
+      assert Room.next_round_player(room) == players["bar"]
+    end
+
+    test "resets to first player after the last one" do
+      players = %{
+        "foo" => Player.new("foo", "foo"),
+        "bar" => Player.new("bar", "bar")
+      }
+
+      room = %{Room.new("room_id") | players: players, round_player: players["bar"]}
+
+      assert Room.next_round_player(room) == players["foo"]
+    end
+  end
+
+  describe "build_next_round/1" do
+    test "builds the next drawing round" do
+      players = %{
+        "foo" => Player.new("foo", "foo"),
+        "bar" => Player.new("bar", "bar")
+      }
+
+      room = %{Room.new("room_id") | players: players} |> Room.build_next_round()
+
+      assert room.round_player == players["foo"]
+      assert %State.Drawing{} = room.state
+    end
+  end
+
+  describe "broadcast_player_message/3" do
+    test "broadcasts message" do
+      players = %{
+        "player_id" => Player.new("player_id", "player name")
+      }
+
+      room = %{Room.new("room_id") | players: players}
+
+      PubSub.room_subscribe("room_id")
+
+      Room.broadcast_player_message(room, "player_id", "hello")
+
+      assert_receive {:new_message,
+                      %PlayerMessage{player_id: "player_id", name: "player name", body: "hello"}}
     end
   end
 end
