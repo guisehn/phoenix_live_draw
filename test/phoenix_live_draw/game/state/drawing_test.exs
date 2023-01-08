d710d901ab557fc80fe4d758e33e5dd637722ec1
Guilherme Sehn <guilherme@hey.com>
2023-01-08 18:27:54 -0300

    add remaining tests for drawing state

--- test/phoenix_live_draw/game/state/drawing_test.exs
+++ test/phoenix_live_draw/game/state/drawing_test.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixLiveDraw.Game.State.DrawingTest do
   use ExUnit.Case, async: true
 
-  alias PhoenixLiveDraw.Game.{Player, PlayerMessage, PubSub, Room, State}
+  alias PhoenixLiveDraw.Game.{Player, PlayerMessage, PubSub, Room, State, SystemMessage}
 
   defp room_in_drawing_state(state_data \\ %{}) do
     room = Room.new("room_id")
@@ -55,18 +55,158 @@ defmodule PhoenixLiveDraw.Game.State.DrawingTest do
     end
   end
 
-  describe "handle_command: draw" do
-    # TODO: broadcasts drawing
-    # TODO: doesn't do anything when player is not the drawer
+  describe "handle_command: {:draw, path}" do
+    test "broadcasts drawing of round player" do
+      PubSub.room_subscribe("room_id")
+
+      room = room_in_drawing_state()
+
+      assert {:ok, nil, ^room} =
+               State.Drawing.handle_command(
+                 room,
+                 "player1_id",
+                 {:draw, [%{x: 3, y: 2}, %{x: 4, y: 1}]}
+               )
+
+      assert_receive {:draw, [%{x: 3, y: 2}, %{x: 4, y: 1}]}
+    end
+
+    test "ignores when player issuing command is not the round player" do
+      PubSub.room_subscribe("room_id")
+
+      room = room_in_drawing_state()
+
+      assert {:ok, nil, ^room} =
+               State.Drawing.handle_command(
+                 room,
+                 "player2_id",
+                 {:draw, [%{x: 3, y: 2}, %{x: 4, y: 1}]}
+               )
+
+      refute_receive {:draw, _}
+    end
   end
 
   describe "handle_message/3" do
-    # TODO: doesn't allow drawer to send message
-    # TODO: broadcasts message when message is not the answer
-    # TODO: doesn't broadcast message when message is the answer
-    # TODO: gives point to drawer and guesser (first hit)
-    # TODO: gives point to drawer and guesser (second hit)
-    # TODO: gives point to drawer and guesser and ends round (everybody hit)
-    # TODO: doesn't give duplicate points
+    test "doesn't allow drawer to send message" do
+      room = room_in_drawing_state()
+
+      PubSub.room_subscribe("room_id")
+
+      assert {:ok, nil, ^room} = State.Drawing.handle_message(room, "player1_id", "hello")
+
+      refute_receive {:new_message, _}
+    end
+
+    test "broadcasts message when message is not the answer" do
+      room = room_in_drawing_state()
+
+      PubSub.room_subscribe("room_id")
+
+      assert {:ok, nil, ^room} = State.Drawing.handle_message(room, "player2_id", "hello")
+
+      assert_receive {:new_message,
+                      %PlayerMessage{
+                        player_id: "player2_id",
+                        name: "player 2",
+                        body: "hello"
+                      }}
+    end
+
+    test "doesn't broadcast player message when message is the answer" do
+      room = room_in_drawing_state()
+
+      PubSub.room_subscribe("room_id")
+
+      assert {:ok, :correct, _room} = State.Drawing.handle_message(room, "player2_id", "cat")
+
+      refute_receive {:new_message, %PlayerMessage{}}
+    end
+
+    test "announces player hit when message is the answer" do
+      room = room_in_drawing_state()
+
+      PubSub.room_subscribe("room_id")
+
+      assert {:ok, :correct, _room} = State.Drawing.handle_message(room, "player2_id", "cat")
+
+      assert_receive {:new_message, %SystemMessage{body: "player 2 hit the answer"}}
+    end
+
+    test "gives point to drawer and guesser (first hit)" do
+      room = room_in_drawing_state()
+
+      PubSub.room_subscribe("room_id")
+
+      assert {:ok, :correct, room} = State.Drawing.handle_message(room, "player2_id", "cat")
+
+      assert room.state.points_earned["player1_id"] == 10
+      assert room.state.points_earned["player2_id"] == 9
+      refute Map.has_key?(room.state.points_earned, "player3_id")
+      refute Map.has_key?(room.state.points_earned, "player4_id")
+
+      assert room.players["player1_id"].points == 0
+      assert room.players["player2_id"].points == 0
+      assert room.players["player3_id"].points == 0
+      assert room.players["player4_id"].points == 0
+    end
+
+    test "gives point to drawer and guesser (second hit)" do
+      room = room_in_drawing_state()
+
+      PubSub.room_subscribe("room_id")
+
+      assert {:ok, :correct, room} = State.Drawing.handle_message(room, "player2_id", "cat")
+      assert {:ok, :correct, room} = State.Drawing.handle_message(room, "player3_id", "cat")
+
+      assert room.state.points_earned["player1_id"] == 10 + 2
+      assert room.state.points_earned["player2_id"] == 9
+      assert room.state.points_earned["player3_id"] == 8
+      refute Map.has_key?(room.state.points_earned, "player4_id")
+
+      assert room.players["player1_id"].points == 0
+      assert room.players["player2_id"].points == 0
+      assert room.players["player3_id"].points == 0
+      assert room.players["player4_id"].points == 0
+    end
+
+    test "gives point to drawer and guesser and ends round when everybody hit" do
+      room = room_in_drawing_state()
+
+      PubSub.room_subscribe("room_id")
+
+      assert {:ok, :correct, room} = State.Drawing.handle_message(room, "player2_id", "cat")
+      assert {:ok, :correct, room} = State.Drawing.handle_message(room, "player3_id", "cat")
+      assert {:ok, :correct, room} = State.Drawing.handle_message(room, "player4_id", "cat")
+
+      assert room.state.__struct__ == State.PostRound
+      assert %{outcome: :all_hit, word_was: "cat"} = room.state
+      assert room.players["player1_id"].points == 10 + 2 + 2
+      assert room.players["player2_id"].points == 9
+      assert room.players["player3_id"].points == 8
+      assert room.players["player4_id"].points == 7
+    end
+
+    test "ignores duplicate correct answer" do
+      room = room_in_drawing_state()
+
+      PubSub.room_subscribe("room_id")
+
+      assert {:ok, :correct, room} = State.Drawing.handle_message(room, "player2_id", "cat")
+
+      refute_receive {:new_message, %PlayerMessage{}}
+      assert_receive {:new_message, %SystemMessage{body: "player 2 hit the answer"}}
+
+      assert room.state.points_earned["player1_id"] == 10
+      assert room.state.points_earned["player2_id"] == 9
+
+      assert {:ok, nil, room} = State.Drawing.handle_message(room, "player2_id", "cat")
+
+      refute_receive {:new_message, %PlayerMessage{}}
+      refute_receive {:new_message, %SystemMessage{}}
+
+      assert room.state.points_earned["player1_id"] == 10
+      assert room.state.points_earned["player2_id"] == 9
+    end
   end
 end
